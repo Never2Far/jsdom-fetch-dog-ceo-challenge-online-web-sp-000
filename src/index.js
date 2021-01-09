1eab660837763d3f8b39c37de9beb3a33a255b54
seth <seth.j.near@gmail.com>
2021-01-08 21:49:04 -0600

    commit tree 8e7bf3e9823e76c99c179dff1ae397c27473e732

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('%c HI', 'color: firebrick')
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const dogImgContainer = document.getElementById("dog-image-container");
+    let dogImg;
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response => response.json())
+.then(data => displayDogs(data['message']));
+
+function displayDogs(data) {
+        data.forEach(element => {
+            dogImg = document.createElement('img')
+           dogImg.setAttribute('src', element)
+           dogImgContainer.appendChild(dogImg);
+        });
+}
+});
